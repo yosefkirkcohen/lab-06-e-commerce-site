438d85e67319b8b86bbbfd0d873e65a61cb3c500
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-23 14:08:29 -0700

    inital commit

---
+++ products/cereals.js
@@ -0,0 +1,42 @@
+const products = [
+    {
+        id: 1,
+        name: 'Wheat Chex',
+        image: 'img',
+        description: 'Baked, square shaped cereal',
+        category: 'wheat',
+        price: 4.00
+    }
+    {
+        id: 2
+        name: 'Life',
+        image: 'img',
+        description: 'Tasty squares of whole grain',
+        category: 'whole grain',
+        price: 4.00
+    }
+    {
+        id: 3
+        name: 'Kix',
+        image: 'img',
+        description: 'Expanded puffed-grain cereal',
+        category: 'cornmeal',
+        price: 4.00
+    }
+    {
+        id: 4
+        name: 'Rice Krispies',
+        image: 'img',
+        description: 'Crunchy and crisp bits',
+        category: 'rice',
+        price: 4.00
+    }
+    {
+        id: 5
+        name: 'Cheerios',
+        image: 'img',
+        description: 'Shaped like a torus',
+        category: 'oats',
+        price: 4.00
+    }
+];
