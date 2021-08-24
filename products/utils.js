4b144c320c8c9543c05b36386655ce3fa366152e
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-23 17:12:51 -0700

    add css

--- products/utils.js
+++ products/utils.js
@@ -4,12 +4,13 @@ export function renderItem(cereal) {
     const image = document.createElement('img');
     const description = document.createElement('p');
     const price = document.createElement('p');
+    const addButton = document.createElement('button');
 
-    li.append(cerealName, image, description, price);
+    li.append(cerealName, image, description, price, addButton);
     cerealName.textContent = cereal.name;
     image.src = cereal.image;
     description.textContent = cereal.description;
     price.textContent = `$${cereal.price}`;
-
+    addButton.textContent = 'Add';
     return li;
 }
