3445adda3ec7785723688b7624322569d13a7643
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-27 16:38:47 -0700

    its working

--- cart/cart.js
+++ cart/cart.js
@@ -1,18 +1,19 @@
-import { products } from '../products/cereals.js';
-import { cart } from './cart-data.js';
+import { getCart } from '../local-storage-utils.js';
+import { getProducts } from '../local-storage-utils.js';
+
 import { renderTableRow } from './render-table-row.js';
 import { calcOrderTotal } from '../utils.js';
 
 const cartTBody = document.getElementById('cart-t-body');
 
-for (let item of cart) {
+for (let item of getCart()) {
     const tr = renderTableRow(item);
 
 
     cartTBody.append(tr);
 }
 
-const orderTotal = calcOrderTotal(cart, products);
+const orderTotal = calcOrderTotal(getCart(), getProducts());
 
 const orderTotalTd = document.getElementById('order-total');
 
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+import { getProducts } from './local-storage-utils.js';
+
 export function findById(someArray, someId) {
     for (let item of someArray) {
         if (someId === item.id) {
@@ -10,10 +12,10 @@ export function calcItemTotal(price, quantity) {
     return price * quantity;
 }
 
-export function calcOrderTotal(cartArray, productsArray) {
+export function calcOrderTotal(cartArray, productArray) {
     let total = 0;
     for (let item of cartArray) {
-        const product = findById(productsArray, item.id);
+        const product = findById(productArray, item.id);
         total = total + calcItemTotal(product.price, item.quantity);
     }
     return total;
